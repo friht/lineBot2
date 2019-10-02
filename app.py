5cb86a3531b522023263357916d2293eb4ed4cdb
friht <51777374+friht@users.noreply.github.com>
2019-10-02 14:22:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,6 @@ def callback():
     decoded = json.loads(json_line)
     user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']
     userText = decoded['queryResult']['intent']['displayName']
-    #sendText(user,userText)
     if (userText == 'กินข้าวหรือยังครับ') :
        sendText(user, 'ยังครับ')
     elif (userText == 'ไปเที่ยวกันไหม') :
